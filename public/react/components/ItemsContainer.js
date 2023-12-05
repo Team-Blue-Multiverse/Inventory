ccc6e650b513afd33dcce6b2a5ef6e0e6c49e4dc
hector <hpulido1021@gmail.com>
2023-12-05 16:29:02 -0500

    Fixed update route

--- public/react/components/ItemsContainer.js
+++ public/react/components/ItemsContainer.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState}, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import apiURL from '../api';
 import { Link } from 'react-router-dom'
 import apiURL from '../api';
