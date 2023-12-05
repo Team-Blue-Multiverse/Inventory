6dfcd7dd1dec5b736d55e0fa224647d4f612c5b8
kaikaine <kairijb@protonmail.com>
2023-12-05 13:10:30 -0600

    added test for get all items route

--- server/routes/index.test.js
+++ server/routes/index.test.js
@@ -8,3 +8,18 @@ const { sequelize } = require("../db");
 const { Item } = require("../models/Item");
 const app = require("../app");
 const seedData = require("../seedData");
+
+describe("items endpoints", () => {
+  test("./items get all", async () => {
+    const res = await request(app).get("/api/items");
+    expect(res.statusCode).toBe(200);
+
+    const resData = res.text;
+
+    let allItems = await Item.findAll();
+
+    allItems = JSON.stringify(allItems);
+
+    expect(resData).toMatch(allItems);
+  });
+});
